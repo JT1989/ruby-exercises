d235b07c43bcc388204047bae27e2643a3efe148
JT1989 <jtiao@kissmetrics.com>
2015-01-25 13:52:16 -0800

    Added count_max.rb

--- exercises/count_max.rb
+++ exercises/count_max.rb
@@ -16,14 +16,25 @@ require_relative './max'
 require_relative './count_in_list'
 
 def count_max(list)
-  # You can write this using nothing more than the max and count_in_list
+	maximum_so_far = max(list) 										#take the list and look for the highest number.
+	result = count_in_list(list, maximum_so_far)	#count how many times this highest number shows in the list
+end
+  #pro-tip: understand signature
+  #You can write this using nothing more than the max and count_in_list
   # methods that you've already written.  You do not HAVE to, but it's worth
   # trying. The "requite_relative" statements above make them available to us, here.
   #
   # But remember: inelegant, working code is better than elegant, unfinished code.
-end
+
 
 if __FILE__ == $0
+	p count_max([10, 1,2,10,10]) == 3
+	p count_max([100, 0,5,-1,6.6]) == 1
+	p count_max([100, 20, 1, 1, 1, 300, 2, 300]) == 2
+	p count_max([8.0, 4.1, 5.4, 2, 8.7]) == 1
+	p count_max([0]) == 1
+	p count_max([1]) == 1
+	p count_max([]) == 0
   # I'd advise putting some sanity checks here.
   # How else will you be sure your code does what you think it does?
 end
