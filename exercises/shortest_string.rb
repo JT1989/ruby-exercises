7d04d9f7d26dc51e6e6ff277200f8a1d24219530
JT1989 <jtiao@kissmetrics.com>
2015-01-25 13:22:55 -0800

    Added file.rb

--- exercises/shortest_string.rb
+++ exercises/shortest_string.rb
@@ -3,11 +3,27 @@
 # Returns:     the shortest string in the list
 # Prints:      Nothing
 
+# For example, shortest_string(["a", "zzzz", "cd"]) should return "a" since
 def shortest_string(list)
-  # This is your job. :)
+	shortest_string_so_far = list.first
+	list.each do |item|
+		if shortest_string_so_far.length < item.length
+			then shortest_string_so_far == item
+		end
 end
+return shortest_string_so_far
+end
+	#look at the entire list, make the first element equal to the shortest string
+	#look at the second one and see if it's < the shortest string so far.
+	# if it is the shortest one, then thats the new shortest string.
+  # This is your job. :)
 
 if __FILE__ == $0
+
+	p shortest_string(["a","zzzz","cd"]) == "a"
+	p shortest_string(["apple", "kitchen", "shadows"]) == "apple"
+	p shortest_string(["dog", "mice", "cats"]) == "dog"
+	p shortest_string(["bomb","diggity","diggity"]) == "bomb"
   # I'd advise putting some sanity checks here.
   # How else will you be sure your code does what you think it does?
 end
