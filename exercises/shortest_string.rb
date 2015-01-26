16c3bea084b3e896ee09f766c1e6d19a1f8de566
JT1989 <jtiao@kissmetrics.com>
2015-01-25 19:53:46 -0800

    Removed 'then' from line 11

--- exercises/shortest_string.rb
+++ exercises/shortest_string.rb
@@ -8,7 +8,7 @@ def shortest_string(list)							#define the method
 	shortest_string_so_far = list.first #look at the entire list, make the first element equal to the shortest string
 	list.each do |string|									#go through each item in the list, place it in the |placeholder| area
 		if string.length < shortest_string_so_far.length #if the length of the placeholder < the shortest string so far.
-			then shortest_string_so_far = string#then the new shortest item is the item
+			shortest_string_so_far = string #then the new shortest item is the item
 		end
 	end
 	return shortest_string_so_far
